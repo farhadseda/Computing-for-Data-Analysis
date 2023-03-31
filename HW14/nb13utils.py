a6721f59e62074ec81974a5ea2d8a4b3fbc57cd2
Farhad Sedaghati <100705813+farhadseda@users.noreply.github.com>
2023-03-30 17:19:50 -0700

    Add files via upload

---
+++ HW14/nb13utils.py
@@ -0,0 +1,67 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Helper functions from Logistic Regression Lesson
+def make_scatter_plot(df, x="x_1", y="x_2", hue="label",
+                      palette={0: "orange", 1: "blue"},
+                      size=5,
+                      centers=None):
+    sns.lmplot(x=x, y=y, hue=hue, data=df, palette=palette,
+               fit_reg=False)
+    if centers is not None:
+        plt.scatter(centers[:,0], centers[:,1],
+                    marker=u'*', s=500,
+                    c=[palette[0], palette[1]])
+    
+def mark_matches(a, b, exact=False):
+    """
+    Given two Numpy arrays of {0, 1} labels, returns a new boolean
+    array indicating at which locations the input arrays have the
+    same label (i.e., the corresponding entry is True).
+    
+    This function can consider "inexact" matches. That is, if `exact`
+    is False, then the function will assume the {0, 1} labels may be
+    regarded as the same up to a swapping of the labels. This feature
+    allows
+    
+      a == [0, 0, 1, 1, 0, 1, 1]
+      b == [1, 1, 0, 0, 1, 0, 0]
+      
+    to be regarded as equal. (That is, use `exact=False` when you
+    only care about "relative" labeling.)
+    """
+    assert a.shape == b.shape
+    a_int = a.astype(dtype=int)
+    b_int = b.astype(dtype=int)
+    all_axes = tuple(range(len(a.shape)))
+    assert ((a_int == 0) | (a_int == 1)).all()
+    assert ((b_int == 0) | (b_int == 1)).all()
+    
+    exact_matches = (a_int == b_int)
+    if exact:
+        return exact_matches
+
+    assert exact == False
+    num_exact_matches = np.sum(exact_matches)
+    if (2*num_exact_matches) >= np.prod (a.shape):
+        return exact_matches
+    return exact_matches == False # Invert
+    
+def count_matches(a, b, exact=False):
+    """
+    Given two sets of {0, 1} labels, returns the number of mismatches.
+    
+    This function can consider "inexact" matches. That is, if `exact`
+    is False, then the function will assume the {0, 1} labels may be
+    regarded as similar up to a swapping of the labels. This feature
+    allows
+    
+      a == [0, 0, 1, 1, 0, 1, 1]
+      b == [1, 1, 0, 0, 1, 0, 0]
+      
+    to be regarded as equal. (That is, use `exact=False` when you
+    only care about "relative" labeling.)
+    """
+    matches = mark_matches(a, b, exact=exact)
+    return np.sum(matches)
